bcb22b7e9972923d95d958bd9776421c91cae9a0
Nildo Silva <nildotoni@gmail.com>
2021-03-24 10:46:57 -0300

    Subindo projeto para o git

---
+++ src/main/java/br/com/ragegames/streetruasv/StreetruasvApplication.java
@@ -0,0 +1,13 @@
+package br.com.ragegames.streetruasv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetruasvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetruasvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ragegames/streetruasv/StreetruasvApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ragegames.streetruasv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetruasvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
